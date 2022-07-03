6dc64404fb8852921d0259053162ba189ea323bd
suribabu <suribabu.rocketmail.com>
2022-07-03 11:33:27 +0530

    firstcommit

---
+++ main.tf
@@ -0,0 +1,75 @@
+provider "aws" {
+  region  = "us-east-1"
+ } 
+
+terraform {
+  backend "s3" {
+    bucket = "abn-it"
+    key    = "devops24workspace.tfstate"
+    region = "us-east-1"
+  }
+}
+resource "aws_vpc" "default" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  tags = {
+    Name        = "${var.vpc_name}"
+    Owner       = "surya.m"
+    environment = "${var.env}"
+  }
+}
+
+resource "aws_internet_gateway" "default" {
+  vpc_id = aws_vpc.default.id
+  tags = {
+    Name = "${var.IGW_name}"
+  }
+}
+
+resource "aws_subnet" "subnet1-public" {
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = var.public_subnet1_cidr
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "${var.public_subnet1_name}"
+  }
+}
+
+resource "aws_route_table" "terraform-public" {
+  vpc_id = aws_vpc.default.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.default.id
+  }
+
+  tags = {
+    Name = "${var.Public_Routing_Table_name}"
+  }
+}
+
+resource "aws_route_table_association" "terraform-public" {
+  subnet_id      = aws_subnet.subnet1-public.id
+  route_table_id = aws_route_table.terraform-public.id
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+  vpc_id      = aws_vpc.default.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
